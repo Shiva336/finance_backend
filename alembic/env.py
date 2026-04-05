6b4cc3fb15ca37635d212042a80d880c2a5e4473
Shiva336 <shivasundar911@gmail.com>
2026-04-05 23:09:23 +0530

    change run migrations online method to sync

--- alembic/env.py
+++ alembic/env.py
@@ -57,16 +57,20 @@ def do_run_migrations(connection: Connection):
 # ----------------------------
 # ONLINE MODE (ASYNC)
 # ----------------------------
-async def run_migrations_online():
-    connectable = create_async_engine(
-        config.get_main_option("sqlalchemy.url"),
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata)
 
-    await connectable.dispose()
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 # ----------------------------
