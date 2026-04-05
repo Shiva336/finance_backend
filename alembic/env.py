06704a74f5c6a58f2a652176cf9069f63eca8c22
Shiva336 <shivasundar911@gmail.com>
2026-04-05 23:11:49 +0530

    redo change

--- alembic/env.py
+++ alembic/env.py
@@ -57,20 +57,16 @@ def do_run_migrations(connection: Connection):
 # ----------------------------
 # ONLINE MODE (ASYNC)
 # ----------------------------
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-def run_migrations_online():
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
+async def run_migrations_online():
+    connectable = create_async_engine(
+        config.get_main_option("sqlalchemy.url"),
         poolclass=pool.NullPool,
     )
 
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
 
-        with context.begin_transaction():
-            context.run_migrations()
+    await connectable.dispose()
 
 
 # ----------------------------
